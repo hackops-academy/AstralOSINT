dd9be6e2d8e254b084c662bb385c87a0c01bf8cf
Rajput Lekhraj Singh <hackops59@gmail.com>
2026-07-11 12:01:33 +0530

    Delete js/menu.js

--- js/menu.js
+++
@@ -1,9 +0,0 @@
- const menuBtn = document.getElementById("menu-btn");
-const sideMenu = document.getElementById("side-menu");
-
-let menuOpen = false;
-
-menuBtn.onclick = () => {
-    menuOpen = !menuOpen;
-    sideMenu.style.left = menuOpen ? "0px" : "-260px";
-};
